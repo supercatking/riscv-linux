c6aa9572aac0fa1ca922b39301aa4af50c93a918
Sagar Shrikant Kadam <sagar.kadam@sifive.com>
2019-07-02 16:29:09 +0530

    mtd: spi-nor: fix nor->addr_width for is25wp256
    Use the post bfpt fixup hook for the is25wp256 device as done for
    is25lp256 device to overwrite the address width advertised by BFPT.
    
    For instance the standard devices eg: IS25WP256D-JMLE where J stands
    for "standard" does not support SFDP.
    
    Signed-off-by: Sagar Shrikant Kadam <sagar.kadam@sifive.com>

--- drivers/mtd/spi-nor/spi-nor.c
+++ drivers/mtd/spi-nor/spi-nor.c
@@ -1860,7 +1860,7 @@ static const struct flash_info spi_nor_ids[] = {
 	{ "is25wp256", INFO(0x9d7019, 0, 64 * 1024, 1024,
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_4B_OPCODES)
-	},
+			.fixups = &is25lp256_fixups },
 	/* Macronix */
 	{ "mx25l512e",   INFO(0xc22010, 0, 64 * 1024,   1, SECT_4K) },
 	{ "mx25l2005a",  INFO(0xc22012, 0, 64 * 1024,   4, SECT_4K) },
