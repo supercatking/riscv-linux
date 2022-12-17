17ffb0935ea4ff4589934150e067f03954254bac
Andy Chiu <andy.chiu@sifive.com>
2022-12-17 16:11:30 +0000

    internal: internal QEMU does not support ENVCFG CSR

--- arch/riscv/kvm/vcpu.c
+++ arch/riscv/kvm/vcpu.c
@@ -868,6 +868,7 @@ int kvm_arch_vcpu_ioctl_set_guest_debug(struct kvm_vcpu *vcpu,
 	return -EINVAL;
 }
 
+/*
 static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
 {
 	u64 henvcfg = 0;
@@ -886,6 +887,7 @@ static void kvm_riscv_vcpu_update_config(const unsigned long *isa)
 	csr_write(CSR_HENVCFGH, henvcfg >> 32);
 #endif
 }
+*/
 
 void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 {
@@ -901,7 +903,7 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 	csr_write(CSR_HVIP, csr->hvip);
 	csr_write(CSR_VSATP, csr->vsatp);
 
-	kvm_riscv_vcpu_update_config(vcpu->arch.isa);
+//	kvm_riscv_vcpu_update_config(vcpu->arch.isa);
 
 	kvm_riscv_gstage_update_hgatp(vcpu);
 
