13ef91a9725fa30633af7c5739089dd34f8f8d33
Mark Brown <broonie@kernel.org>
2023-10-09 13:08:36 +0100

    prctl: arch-agnostic prctl for shadow stack
    Three architectures (x86, aarch64, riscv) have announced support for
    shadow stacks with fairly similar functionality.  While x86 is using
    arch_prctl() to control the functionality neither arm64 nor riscv uses
    that interface so this patch adds arch-agnostic prctl() support to
    get and set status of shadow stacks and lock the current configuration to
    prevent further changes, with support for turning on and off individual
    subfeatures so applications can limit their exposure to features that
    they do not need.  The features are:
    
      - PR_SHADOW_STACK_ENABLE: Tracking and enforcement of shadow stacks,
        including allocation of a shadow stack if one is not already
        allocated.
      - PR_SHADOW_STACK_WRITE: Writes to specific addresses in the shadow
        stack.
      - PR_SHADOW_STACK_PUSH: Push additional values onto the shadow stack.
      - PR_SHADOW_STACK_DISABLE: Allow to disable shadow stack.
        Note once locked, disable must fail.
    
    These features are expected to be inherited by new threads and cleared
    on exec(), unknown features should be rejected for enable but accepted
    for locking (in order to allow for future proofing).
    
    This is based on a patch originally written by Deepak Gupta but later
    modified by Mark Brown for arm's GCS patch series.
    
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Co-developed-by: Deepak Gupta <debug@rivosinc.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -4209,5 +4209,8 @@ static inline bool pfn_is_unaccepted_memory(unsigned long pfn)
 
 	return range_contains_unaccepted_memory(paddr, paddr + PAGE_SIZE);
 }
+int arch_get_shadow_stack_status(struct task_struct *t, unsigned long __user *status);
+int arch_set_shadow_stack_status(struct task_struct *t, unsigned long status);
+int arch_lock_shadow_stack_status(struct task_struct *t, unsigned long status);
 
 #endif /* _LINUX_MM_H */
